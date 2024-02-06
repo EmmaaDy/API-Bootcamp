2ffc30f18e629e04531cd1efa5e1521efa0a2c1e
Emma <emma.dybdorf@hotmail.com>
2024-02-06 11:15:47 +0100

    La till Loop

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ fetch ('https://fe23-folkuniversitetet.github.io/API/Data/pokemons.json')
     .then(response => response.json())
     .then(data => {
         console.log(data);
-    })
+        
+        data.forEach(function(pokemon) {
+            console.log(pokemon.name);
+          });
+        })
     .catch(error => {
         console.error('Det uppstod ett fel:', error);
     });
